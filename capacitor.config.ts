33c47c2a7d5b228b304dacba3367c972e84f7194
ARTHUR CARDOSO <arthur.cardoso1@alunos.sc.senac.br>
2025-06-25 09:14:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ATV-GERENCIADOR-DE-TAREFAS',
+  webDir: 'www'
+};
+
+export default config;
